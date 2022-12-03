c73dfabce0e2f0612b455dbdf56b3a2455718f1c
Laura McGowan <prime.lmcg@gmail.com>
2022-12-03 08:00:24 -0600

    commented out a todo list and got my table up and styled

---
+++ assignment/scripts/client.js
@@ -0,0 +1,39 @@
+/*
+To Do:
+
+
+*/
+
+
+$(document).ready(onReady);
+
+
+function onReady(){
+//Make a click handler for the #submitEmployeeInfoButton
+$('#submitEmployeeInfoButton').on('click', addEmployeeInfo);
+//Make a click handler for the .deleteEmployeeInfoButton
+$('body').on('click', '.deleteEmployeeInfobutton', deleteEmployeeInfo)
+}
+
+
+
+////////// DELETING /////////
+
+//Function to delete an employee and their information 
+
+//Function to render the employee list
+
+
+
+////////// ADDING /////////
+
+//Function to render the employee information
+
+
+//Function to add new employee and their information
+
+//Get input values
+
+//Add to array
+
+//Set inputs back to empty 
