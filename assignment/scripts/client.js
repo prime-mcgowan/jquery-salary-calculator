323afc5dcaa2df94405a6a9dcc06d4f2f32f7d3a
Laura McGowan <prime.lmcg@gmail.com>
2022-12-03 09:26:55 -0600

    my new employee data rendersgit add .git add .

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -4,16 +4,24 @@ To Do:
 
 */
 
-let employeeInfo = []   
+let employee = []
+
+    /*{first: '',
+    last: '',
+    id: [],
+    title: '',
+    salary: []
+}
+*/
 
 
 $(document).ready(onReady);
 
 
 function onReady(){
-    console.log('in onReady Function')
+    renderEmployeeList()
 //Make a click handler for the #submitEmployeeInfoButton
-//$('#submitEmployeeInfoButton').on('click', addEmployeeInfo);
+$('#submitEmployeeInfoButton').on('click', addEmployeeInfo);
 //Make a click handler for the .deleteEmployeeInfoButton
 //$('body').on('click', '.deleteEmployeeInfobutton', deleteEmployeeInfo)
 }
@@ -24,44 +32,56 @@ function onReady(){
 
 //Function to delete an employee and their information 
 
-//Function to render the employee list
+//Function to render the employee list after deletion
 
 
 
 ////////// ADDING /////////
 
 //Function to render the employee information
+function renderEmployeeList () {
+$('#employeeData').empty();
+for(let i=0; i<employee.length; i++){
+    $('#employeeData').append(`
 
+   <td> ${employee[i].first}</td>
+    <td>${employee[i].last}</td>
+    <td>${employee[i].id}</td>
+    <td>${employee[i].title}</td>
+    <td>${employee[i].salary}</td>
+
+    `)
+}
+}
 
 //Function to add new employee and their information
-
 function addEmployeeInfo() {
-    
+
 //Get input values
-let newFirstName = $('firstNameInput').val();
-let newLastName = $('lastNameInput').val();
-let newId = $('idInput').val();
-let newTitle = $('titleInput').val();
-let newAnnualSalary = $('annualSalaryInput').val();
+let newFirst = $('#firstNameInput').val();
+let newLast = $('#lastNameInput').val();
+let newId = $('#idInput').val();
+let newTitle = $('#titleInput').val();
+let newAnnualSalary = $('#annualSalaryInput').val();
 
 //Create new employeeInfo object
 let newEmployeeInfo = {
-    first:  (newFirstName),
-    last:   (newLastName),
+    first:  (newFirst),
+    last:   (newLast),
     id:     Number(newId),
     title:  (newTitle),
     salary: Number(newAnnualSalary)
 }
 
 //Add to array
-employeeInfo.push(newEmployeeInfo);
-//renderEmployeeList();
+employee.push(newEmployeeInfo);
+renderEmployeeList();
 
 //Set inputs back to empty 
-$('firstNameInput').val('');
-$('lastNameInput').val('');
-$('idInput').val('');
-$('titleInput').val('');
-$('annualSalaryInput').val('');
+$('#firstNameInput').val('');
+$('#lastNameInput').val('');
+$('#idInput').val('');
+$('#titleInput').val('');
+$('#annualSalaryInput').val('');
 
-} //ends addEmployeeInfo function
+}//ends addEmployeeInfo function
