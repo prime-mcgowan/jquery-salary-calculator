24f4ea8799b939b2ed6b5e9a794222aaf1a81019
Laura McGowan <prime.lmcg@gmail.com>
2022-12-03 08:26:01 -0600

    coded up: input values, new object, push and emptying inputs

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -4,15 +4,18 @@ To Do:
 
 */
 
+let employeeInfo = []   
+
 
 $(document).ready(onReady);
 
 
 function onReady(){
+    console.log('in onReady Function')
 //Make a click handler for the #submitEmployeeInfoButton
-$('#submitEmployeeInfoButton').on('click', addEmployeeInfo);
+//$('#submitEmployeeInfoButton').on('click', addEmployeeInfo);
 //Make a click handler for the .deleteEmployeeInfoButton
-$('body').on('click', '.deleteEmployeeInfobutton', deleteEmployeeInfo)
+//$('body').on('click', '.deleteEmployeeInfobutton', deleteEmployeeInfo)
 }
 
 
@@ -32,8 +35,33 @@ $('body').on('click', '.deleteEmployeeInfobutton', deleteEmployeeInfo)
 
 //Function to add new employee and their information
 
+function addEmployeeInfo() {
+    
 //Get input values
+let newFirstName = $('firstNameInput').val();
+let newLastName = $('lastNameInput').val();
+let newId = $('idInput').val();
+let newTitle = $('titleInput').val();
+let newAnnualSalary = $('annualSalaryInput').val();
+
+//Create new employeeInfo object
+let newEmployeeInfo = {
+    first:  (newFirstName),
+    last:   (newLastName),
+    id:     Number(newId),
+    title:  (newTitle),
+    salary: Number(newAnnualSalary)
+}
 
 //Add to array
+employeeInfo.push(newEmployeeInfo);
+//renderEmployeeList();
 
 //Set inputs back to empty 
+$('firstNameInput').val('');
+$('lastNameInput').val('');
+$('idInput').val('');
+$('titleInput').val('');
+$('annualSalaryInput').val('');
+
+} //ends addEmployeeInfo function
